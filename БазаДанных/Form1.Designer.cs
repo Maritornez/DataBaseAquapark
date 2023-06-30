ed24107c166598d71ed55faa10eafba140394a27
George Potonov <62441435+Maritornez@users.noreply.github.com>
2023-06-30 21:40:52 +0300

    Add files via upload

---
+++ БазаДанных/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace БазаДанных
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.aquaparkDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.aquaparkDataSet = new БазаДанных.AquaparkDataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aquaparkDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aquaparkDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Подключиться к базе данных";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(462, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(277, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Вывести список клиентов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(462, 86);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 70);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Вывести список записей дохода по убыванию суммы";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(462, 174);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(321, 70);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Вывести список записей о посещении зоны \'Водные аттракционы\'";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(15, 400);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 38);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Назад";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(708, 400);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 38);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Выход";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.aquaparkDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(123, 74);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // aquaparkDataSetBindingSource
+            // 
+            this.aquaparkDataSetBindingSource.DataSource = this.aquaparkDataSet;
+            this.aquaparkDataSetBindingSource.Position = 0;
+            // 
+            // aquaparkDataSet
+            // 
+            this.aquaparkDataSet.DataSetName = "AquaparkDataSet";
+            this.aquaparkDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Клиентское приложение";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aquaparkDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aquaparkDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource aquaparkDataSetBindingSource;
+        private БазаДанных.AquaparkDataSet aquaparkDataSet;
+    }
+}
+
---
+++ БазаДанных/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace БазаДанных
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Visible = true;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            label1.Visible = false;
+            dataGridView1.Visible = false;
+
+            dataGridView1.AutoSize = true;
+            dataGridView1.AutoGenerateColumns = true;
+        }
+        string connectionString = @"Data Source=HASEE-ZX6\SQLEXPRESS; Initial Catalog=Aquapark; Integrated Security=True";
+
+        //запрос: вывести список клиентов
+        string sqlExpression2 = "SELECT gender_code AS 'Пол (М-0, Ж-1)', flag_code AS 'Постоянный клиент', name AS Имя, age AS Возраст FROM client;";
+        //запрос: вывести список записей дохода по убыванию суммы
+        string sqlExpression3 = "SELECT date AS Дата, total AS Сумма FROM bracelet ORDER BY total DESC;";
+        //запрос: вывести список записей о посещении зоны "Водные аттракционы" 
+        string sqlExpression4 = "SELECT minute_amount AS 'Время (мин)', total AS Сумма FROM payment WHERE zone_code = 0 ORDER BY minute_amount;";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                button1.Visible = false;
+                button2.Visible = true;
+                button3.Visible = true;
+                button4.Visible = true;
+                button7.Visible = true;
+                label1.Visible = true;
+                //Основные сведения о подключении
+                label1.Text = "Подключение установлено\n\n" +
+                              "Свойства подключения:\n" +
+                              "    База данных: " + connection.Database + "\n" +
+                              "    Сервер: " + connection.DataSource + "\n" +
+                              "    Версия сервера: " + connection.ServerVersion + "\n" +
+                              "    Состояние: " + connection.State + "\n" +
+                              "    Рабочая станция: " + connection.WorkstationId + "\n";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Visible = false;
+                button6.Visible = true;
+                button7.Visible = true;
+                label1.Visible = false;
+                dataGridView1.Visible = true;
+                DataSet ds = new DataSet();
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlExpression2, connection);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+                adapter.Fill(ds, "client");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "client";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Visible = false;
+                button6.Visible = true;
+                label1.Visible = false;
+                dataGridView1.Visible = true;
+                DataSet ds = new DataSet();
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlExpression3, connection);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+                adapter.Fill(ds, "bracelet");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "bracelet";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Visible = false;
+                button6.Visible = true;
+                label1.Visible = false;
+                dataGridView1.Visible = true;
+                DataSet ds = new DataSet();
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlExpression4, connection);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+                adapter.Fill(ds, "payment");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "payment";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //Вернуться назад в меню
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+            button6.Visible = false;
+            label1.Visible = true;
+            dataGridView1.Visible = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //Выход
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Close();
+                this.Close();
+            }
+        }
+    }
+}
